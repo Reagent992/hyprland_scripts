6cc886a3698a2a901ec44a31eca3ed50003cb556
Miron Sadykov <MironSadykov@yandex.ru>
2025-06-14 23:58:28 +0300

    feat: add another version of run_or_focus script

--- docs/run_or_focus.md
+++ docs/run_or_focus.md
@@ -14,14 +14,16 @@ A utility script for **Hyprland** that ensures only one instance of a specific a
 - `hyprctl` (should come with Hyprland)
 - `notify-send` (typically provided by `libnotify`)
 
+`run_or_focus_hyprpy.py` it another version of the same script. It has **uv** as a dependency and uses the `hyprpy` lib. It was created just for fun.
+
 ## 🚀 Installation
 
 1. Clone the repository or download the script and make it executable:
 
 ```bash
-git clone https://github.com/yourusername/hypr-single-launch.git
-cd hypr-single-launch
-chmod +x run_or_focus.py
+git clone https://github.com/Reagent992/hyprland_scripts.git
+cd hyprland_scripts/run_or_focus
+chmod u+x run_or_focus.py
 ```
 
 ## 🛠️ Usage
@@ -32,7 +34,7 @@ run_or_focus.py [workspace] application [application arguments]
 
 ```ini
 # hypr.conf
-$run_or_focus = /path/to/run_or_focus.py
-bind = SUPER, O, exec, run_or_focus 4 obsidian
-bind = SUPER, C, exec, run_or_focus 2 code --profile blank_profile
+$open = /path/to/run_or_focus.py
+bind = SUPER, O, exec, $open 4 obsidian
+bind = SUPER, C, exec, $open 2 code --profile blank_profile
 ```
---
+++ run_or_focus/run_or_focus_hyprpy.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.12"
+# dependencies = ["hyprpy"]
+# ///
+
+# Author: Sadykov Miron <MironSadykov@yandex.ru>
+# License: MIT
+# 2025
+
+from hyprpy import Hyprland
+
+
+import argparse
+import subprocess
+
+from hyprpy.components.windows import Window
+
+
+DESCRIPTION = """Run a single instance of an application in Hyperland:
+focus it if it's already running,
+or launch it otherwise (optionally on a specific workspace)."""
+
+
+def find_window(looking_window: str, windows: list[Window]) -> Window:
+    return next(filter(lambda x: looking_window in x.wm_class, windows))
+
+
+def focus_window(window: Window, hyprland_instance: Hyprland) -> None:
+    cmd = ["focuswindow", f"class:{window.wm_class}"]
+    hyprland_instance.dispatch(cmd)
+
+
+def open_app(args: argparse.Namespace, hyprland_instance: Hyprland) -> None:
+    cmd = [
+        "exec",
+        f"[workspace {args.workspace}]" if args.workspace is not None else "",
+        args.app_name,
+        *args.rest_args,
+    ]
+    hyprland_instance.dispatch(cmd)
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description=DESCRIPTION,
+    )
+    parser.add_argument(
+        "workspace",
+        type=int,
+        nargs="?",
+    )
+    parser.add_argument("app_name", type=str, metavar="application")
+    parser.add_argument(
+        "rest_args",
+        metavar="APP ARGS",
+        nargs=argparse.REMAINDER,
+        help="Additional arguments passed to the application",
+    )
+    return parser.parse_args()
+
+
+def send_notification(notificatin_msg: str, app_name: str) -> None:
+    subprocess.run(
+        (
+            "notify-send",
+            f"-a {app_name}",
+            f"[{app_name}] Failed to launch or focus",
+            notificatin_msg,
+        ),
+        stdout=subprocess.DEVNULL,
+    )
+
+
+def main() -> None:
+    args = get_args()
+    try:
+        hyprland_instance = Hyprland()
+        windows = hyprland_instance.get_windows()
+        try:
+            window = find_window(args.app_name, windows)
+            focus_window(window, hyprland_instance)
+        except StopIteration:
+            open_app(args, hyprland_instance)
+    except Exception as e:
+        send_notification(str(e), args.app_name)
+
+
+if __name__ == "__main__":
+    main()
